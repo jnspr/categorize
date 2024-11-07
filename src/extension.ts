858f9b730bce413e8c747b9a02a46c53ee798584
Jonas Sprenger <jnspr@outlook.de>
2024-11-07 22:11:53 +0100

    add extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,129 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "categorize" is now active!');
+const iconCache = new Map<string, vscode.ThemeIcon>();
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "categorize.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from categorize!");
+/** Description of a category of exclusion patterns */
+export type Category = {
+  label: string | null;
+  patterns: string[];
+  detail: string | null;
+  iconName: string | null;
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  /** Gets a theme icon by name and caches it for later use */
+  const getIconByName = (name: string): vscode.ThemeIcon | null => {
+    let icon = iconCache.get(name);
+    if (icon === undefined) {
+      if (iconCache.size >= 128) {
+        // Keep the cache small
+        iconCache.clear();
+      }
+      icon = new vscode.ThemeIcon(name);
+      iconCache.set(name, icon);
     }
+    return icon;
+  };
+
+  /** Asks the user to pick a category */
+  const pickCategory = async (
+    configuration: vscode.WorkspaceConfiguration
+  ): Promise<string | null> => {
+    // Get the current set of configured categories
+    let categories = configuration.get<Record<string, Category>>(
+      "categorize.categories"
+    );
+    if (typeof categories !== "object" || !categories) {
+      categories = {};
+    }
+
+    // Convert valid entries into quick pick items
+    const showAllItem = {
+      key: null,
+      label: "Show all files",
+      detail: "Disables any active category",
+      description: "null",
+      iconPath: getIconByName("explorer-view-icon	") ?? undefined,
+    };
+    const categoryItems = Object.entries(categories).map(
+      ([name, category]): vscode.QuickPickItem & {
+        key: string | null;
+      } => ({
+        label: category.label ? category.label : name,
+        description: category.label ? name : undefined,
+        detail: category.detail ?? undefined,
+        iconPath: category.iconName
+          ? (getIconByName(category.iconName) ?? undefined)
+          : undefined,
+        key: name,
+      })
+    );
+
+    // Perform the quick pick action and return the resulting category's name
+    const { key = null } =
+      (await vscode.window.showQuickPick([showAllItem, ...categoryItems])) ??
+      {};
+    return key;
+  };
+
+  /** Applies the currently configured exclusion category */
+  const applyCurrentCategory = async (
+    configuration: vscode.WorkspaceConfiguration,
+    currentCategory: string | null
+  ) => {
+    // Read the current configuration
+    let categories = configuration.get<Record<string, Category>>(
+      "categorize.categories"
+    );
+    if (typeof categories !== "object" || !categories) {
+      categories = {};
+    }
+
+    // Merge the always excluded and conditionally excluded patterns
+    const category = currentCategory ? categories[currentCategory] : undefined;
+    const patterns = category ? category.patterns : [];
+
+    // Merge the unconditionally and conditionally excluded files into the VSCode `files.exclude` setting
+    const filesExclude = {
+      ...(configuration.get<Record<string, boolean>>(
+        "categorize.excludeAlways"
+      ) ?? {}),
+      ...Object.fromEntries(patterns.map((pattern) => [pattern, true])),
+    };
+    await configuration.update(
+      "files.exclude",
+      filesExclude,
+      vscode.ConfigurationTarget.Global
+    );
+  };
+
+  // Implent the `Categorize: Pick category` action
+  context.subscriptions.push(
+    vscode.commands.registerCommand("categorize.pickCategory", async () => {
+      const configuration = vscode.workspace.getConfiguration();
+      const nextCategory = await pickCategory(configuration);
+      await configuration.update(
+        "categorize.currentCategory",
+        nextCategory,
+        vscode.ConfigurationTarget.Global
+      );
+      await applyCurrentCategory(configuration, nextCategory);
+    })
   );
 
-  context.subscriptions.push(disposable);
+  // Implement the `Categorize: Force refresh` action
+  context.subscriptions.push(
+    vscode.commands.registerCommand("categorize.forceRefresh", async () => {
+      const configuration = vscode.workspace.getConfiguration();
+      await applyCurrentCategory(
+        configuration,
+        configuration.get<string | null>("categorize.currentCategory") ?? null
+      );
+      vscode.window.showInformationMessage("Category was refreshed.");
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  iconCache.clear();
+}
